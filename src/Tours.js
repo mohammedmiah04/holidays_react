d2836c9ec89adb5dde72faa50e9b64862d5d60c0
MohammedM <nanumiah@nanus-mbp.home>
2021-01-14 12:20:05 +0000

    made holiday list with toggle

---
+++ src/Tours.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Tour from "./Tour";
+
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>Browse Holidays</h2>
+        <div className="underline"></div>
+        <div>
+          {tours.map((tour) => {
+            return <Tour key={tour.id} {...tour}></Tour>;
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
