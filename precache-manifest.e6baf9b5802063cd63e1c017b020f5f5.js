4df63bba3b9a77716746aecf9ed483e8ff0684df
MohammedM <nanumiah@nanus-mbp.home>
2021-01-14 23:58:02 +0000

    Updates

---
+++ precache-manifest.e6baf9b5802063cd63e1c017b020f5f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ade9240076cb1347d9068171cd420f",
+    "url": "/mohammedmiah04/holidays_react/index.html"
+  },
+  {
+    "revision": "d3f9818135a6a07e84e4",
+    "url": "/mohammedmiah04/holidays_react/static/css/main.28eaa912.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/mohammedmiah04/holidays_react/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mohammedmiah04/holidays_react/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f9818135a6a07e84e4",
+    "url": "/mohammedmiah04/holidays_react/static/js/main.a5caf075.chunk.js"
+  },
+  {
+    "revision": "afb2f9749ab524cf117c",
+    "url": "/mohammedmiah04/holidays_react/static/js/runtime-main.f8af6db5.js"
+  }
+]);
